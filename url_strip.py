0986d801593e7d0eaf96959d125bb60fb6618b3f
Andrew Fishberg <fishberg.dev@gmail.com>
2023-03-22 17:50:39 -0400

    added parser logic

--- url_strip.py
+++ url_strip.py
@@ -24,22 +24,17 @@ PARSERS = {
 }
 
 def strip(url):
-    # TODO detect which parser to use
-    return PARSERS[BASE_AMAZON](url)
+    for key in PARSERS.keys():
+        if key in url:
+            return PARSERS[key](url)
+    print('ERROR: no matching parser detected for clipboard contents',file=sys.stderr)
+    sys.exit(1)
 
 ################################################################################
 
-if len(sys.argv) == 1:
-    print('no command line input provided, grabbing input from clipboard')
-    in_url = subprocess.check_output(['xclip', '-o'],universal_newlines=True)
-elif len(sys.argv) == 2:
-    print('command line input')
-    in_url = sys.argv[1]
-else:
-    print('ERROR: incorrect command line arguments',file=sys.stderr)
-    sys.exit(1)
-
+in_url = subprocess.check_output(['xclip', '-o'],universal_newlines=True)
 print(f'input: {in_url}')
+
 out_url = strip(in_url)
 print(f'output: {out_url}')
 
